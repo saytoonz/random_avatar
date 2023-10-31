0c767559d1accdacb17f8a8b9c407885f1da3410
Jack Meng <45053009+exoad@users.noreply.github.com>
2023-10-30 23:20:30 -0400

    FIX: apply changes to comply with dart 3.0

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -8,7 +8,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -23,15 +23,15 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key key, this.title}) : super(key: key);
+  const MyHomePage({super.key, required this.title});
 
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  MyHomePageState createState() => MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class MyHomePageState extends State<MyHomePage> {
   final TextEditingController _controller = TextEditingController();
   final List<Widget> _painters = <Widget>[];
 
